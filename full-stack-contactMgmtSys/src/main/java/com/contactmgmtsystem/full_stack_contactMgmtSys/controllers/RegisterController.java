3b48150916d8f9a5fa3c1c48b794d0c9d0375e0f
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-12-20 20:07:29 +0545

    Receive InputData

--- full-stack-contactMgmtSys/src/main/java/com/contactmgmtsystem/full_stack_contactMgmtSys/controllers/RegisterController.java
+++ full-stack-contactMgmtSys/src/main/java/com/contactmgmtsystem/full_stack_contactMgmtSys/controllers/RegisterController.java
@@ -1,15 +1,18 @@
 package com.contactmgmtsystem.full_stack_contactMgmtSys.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.contactmgmtsystem.full_stack_contactMgmtSys.forms.UserForm;
 
 @Controller
 public class RegisterController {
 
  // Processing register
     @RequestMapping(value="/do-register", method = RequestMethod.POST)
-    public String processRegister(){
+    public String processRegister(@ModelAttribute UserForm userForm){
         System.out.println("procseeing registraiton");
 /*  Steps to follow for the registration:
  1). Fetch the data or Input from the form
@@ -18,7 +21,7 @@ public class RegisterController {
  4). Message if needed 
  5). Redirect to login page
 */
-
+        System.out.println(userForm);
         return "redirect:/register";
     }
 }
