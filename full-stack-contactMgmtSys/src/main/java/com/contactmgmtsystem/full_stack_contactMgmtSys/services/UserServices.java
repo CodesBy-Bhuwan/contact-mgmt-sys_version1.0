a40041474a833d7575fb0dbdffef84a297d0f5a3
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-12-20 23:47:40 +0545

    updateUser in UserServiceImpl

--- full-stack-contactMgmtSys/src/main/java/com/contactmgmtsystem/full_stack_contactMgmtSys/services/UserServices.java
+++ full-stack-contactMgmtSys/src/main/java/com/contactmgmtsystem/full_stack_contactMgmtSys/services/UserServices.java
@@ -10,7 +10,7 @@ public interface UserServices {
     User saveUser(User user);
     // Whenever we want to cl getUserById, then we will get optional data. From this we can see if there already existed data.
     Optional<User> getUserById(String id);
-    Optional<User> updatUser(User user);
+    Optional<User> updateUser(User user);
     void deleteUser(String id);
     boolean isUserExist(String userId);
     boolean isUSerExistByEmail(String email);
