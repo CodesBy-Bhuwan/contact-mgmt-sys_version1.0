f4a152107f247a4ce7bc752afebe6827f89dbaee
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-12-21 19:08:35 +0545

    isUserExistByEmail&&getAllUser

--- full-stack-contactMgmtSys/src/main/java/com/contactmgmtsystem/full_stack_contactMgmtSys/repository/UserRepo.java
+++ full-stack-contactMgmtSys/src/main/java/com/contactmgmtsystem/full_stack_contactMgmtSys/repository/UserRepo.java
@@ -1,5 +1,7 @@
 package com.contactmgmtsystem.full_stack_contactMgmtSys.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,14 @@ import com.contactmgmtsystem.full_stack_contactMgmtSys.entities.User;
 // Repo are used to interact with db
 @Repository
 public interface UserRepo extends JpaRepository<User, String> {
+
+
+
+    // This optional custom method is to find using User using Email keeping email args
+    Optional<User>findByEmail(String email);
+
+    /*In case of creating such optional method to find User using 2 arguments:
+    Eg.: Using Email and Password
+    Optional<User>findByEmailAndPassword(String email, String password); */
 
 }
