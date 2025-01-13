ae14126d262adcb54bff1125b0cc9ff2e0932139
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2025-01-13 23:39:18 +0545

    setUserDetailsService1.0

--- full-stack-contactMgmtSys/src/main/java/com/contactmgmtsystem/full_stack_contactMgmtSys/entities/User.java
+++ full-stack-contactMgmtSys/src/main/java/com/contactmgmtsystem/full_stack_contactMgmtSys/entities/User.java
@@ -1,7 +1,10 @@
 package com.contactmgmtsystem.full_stack_contactMgmtSys.entities;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -9,6 +12,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity(name = "user")
 @Table(name = "user")
@@ -18,7 +24,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
     // Only basic information will be asked;
 
     @Id
@@ -36,7 +42,7 @@ public class User {
     private String password;
     
     // Verification information incase available;
-    private boolean enabled = false;
+    private boolean enabled = true;
     private boolean emailVerified = false;
     private boolean phoneNumberVerified = false;
 
@@ -55,4 +61,52 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     private List<Contact> contacts = new ArrayList<>();
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roleList = new ArrayList<>();
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        /* listed the roles[ADMIN, USER or other] and colletion of SimpleGrantedAuthority{roles(admin,user,other)}*/
+        Collection<SimpleGrantedAuthority> roles= roleList
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role))
+                .collect(Collectors
+                        .toSet());
+
+        return roles;
+    }
+
+    @Override
+//    Here we want to make user to give username as email while loggin' in not username else we can also phoneNumber or username itself as required
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+//        we can make it dynamically too but for now just to test
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+//    this is getter given by UserDetails itself but incase we have already enrolled in above defination section but also need to return dynamically or not assigned in above
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
 }
