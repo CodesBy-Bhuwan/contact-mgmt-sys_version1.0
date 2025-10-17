af2415b7a84d90048c7191eab7afcba230899b4d
CodesBy-Bhuwan <CodesByBhuwan@github.com>
2025-10-17 21:29:43 +0545

    Checking user logged in profile-> Principal

--- full-stack-contactMgmtSys/src/main/java/com/contactmgmtsystem/full_stack_contactMgmtSys/controllers/UserController.java
+++ full-stack-contactMgmtSys/src/main/java/com/contactmgmtsystem/full_stack_contactMgmtSys/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.contactmgmtsystem.full_stack_contactMgmtSys.controllers;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
@@ -27,9 +31,15 @@ public class UserController {
     }
 
 
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping(value = "/profile", method = RequestMethod.GET)
 //    In RequestMethod default method is GET
-    public String userProfile(){
+    public String userProfile(Principal principal){
+
+        String name = principal.getName();
+        logger.info( name + "User logged in");
+
         System.out.println("This is userProfile");
         return "user/profile";
     }
